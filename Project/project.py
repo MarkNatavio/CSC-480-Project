4df315958005d72a89333c908f841987c1c62be5
Mark Natavio <43160780+MarkNatavio@users.noreply.github.com>
2021-11-30 13:19:21 -0500

    Fixed Encryption

--- Project/project.py
+++ Project/project.py
@@ -1,8 +1,8 @@
 import random
 
 def enc(g, p, key, m, pk): # El Gamal Encryption Scheme
-  c1 = pow(g,key,mod=p)
+  c1 = pow(g,key,p)
   
-  g2 = pow(g,pk)%p
-  c2 = m*(pow(g2,key,mod=p))
+  g2 = pow(g,pk,p)
+  c2 = (m*(pow(g2,key)))%p
   return c1, c2
