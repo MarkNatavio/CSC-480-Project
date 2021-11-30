e9941c473908174f250fd795f359102b19349848
Mark Natavio <43160780+MarkNatavio@users.noreply.github.com>
2021-11-30 13:30:24 -0500

    Added comments

--- Project/project.py
+++ Project/project.py
@@ -1,6 +1,17 @@
 import random
 
 def enc(g, p, key, m, pk): # El Gamal Encryption Scheme
+  '''
+  Inputs:
+  g - some integer
+  p - prime number
+  key - public key
+  m - message
+  pk - private key (comes from decryption algorithm)
+  
+  Outputs:
+  c1, c2 - Encrypted messages
+  '''
   c1 = pow(g,key,p)
   
   g2 = pow(g,pk,p)
