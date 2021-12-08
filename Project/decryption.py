0f3593ca93b6260592377fb2255607920db9be41
Creed0918 <71793959+Creed0918@users.noreply.github.com>
2021-12-07 20:57:13 -0500

    Update decryption.py

--- Project/decryption.py
+++ Project/decryption.py
@@ -16,3 +16,25 @@ def elgamal_dec(c1,c2,p,sk):
 # test:
 # c1 = 48, c2 = 40, p = 61, sk = 16
 # elgamal_dec(48,40,61,16) = 50
+
+
+
+
+
+#Alternate method
+def normal_version_elgamal_dec(y1,y2,p,rsk):
+
+    """
+    inputs
+    y1: cipytertext y1
+    y2: cipytertext y2
+    p: prime number
+    rsk: receiver's secret key
+
+    output
+    dec_m: decrypted message
+    """
+    dec_m = pow(y1,(p-rsk-1))*y2%p
+    return dec_m
+
+
